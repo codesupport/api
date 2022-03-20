8894b22e6434718827cccb3b46f9270e391fc97d
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-03-20 16:52:34 +0100

    Add Swager UI (#8)
    * Added swagger ui
    
    * Chagned import order

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +7,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiTags('Example Routes')
+  @ApiResponse({ status: 200, description: 'Returns Hello World!'})
   getHello(): string {
     return this.appService.getHello();
   }
