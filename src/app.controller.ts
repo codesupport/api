f3a94beeda888658b73bef9bb4f932ad4ad6cd37
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-03-20 10:17:39 +0100

    Added swagger ui

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiTags('Example Routes')
+  @ApiResponse({ status: 200, description: 'Returns Hello World!'})
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const port = Number.parseInt(process.env.PORT) || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port);
-  console.log(`Now listening on port ${port}`);
+    const app = await NestFactory.create(AppModule);
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Codesupport API')
+        .setVersion("v2")
+        .setDescription('The new and better CodeSupport API')
+        .setContact('LamboCreeper', 'https://codesupport.dev', '')
+        .build();
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, swaggerDocument);
+
+    await app.listen(port);
+    console.log(`Now listening on port ${port}`);
 }
+
 bootstrap();
