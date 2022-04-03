e6c30081afff09b66b5538cdf9c4746b0f1fad89
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2022-04-03 14:23:00 +0100

    Format code to match linter rules.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 import { ApiResponse, ApiTags } from "@nestjs/swagger";
-import { AppService } from './app.service';
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @ApiTags('Example Routes')
-  @ApiResponse({ status: 200, description: 'Returns Hello World!'})
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	@ApiTags("Example Routes")
+	@ApiResponse({status: 200, description: "Returns Hello World!"})
+	getHello(): string {
+		return this.appService.getHello();
+	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,24 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: Number.parseInt(process.env.DATABASE_PORT) || 5432,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASS,
-      database: process.env.DATABASE_NAME,
-      entities: [
-        __dirname + '/**/*.entity{.ts,.js}',
-      ],
-      synchronize: process.env.NODE_ENV !== 'production',
-    })
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: "postgres",
+			host: process.env.DATABASE_HOST,
+			port: Number.parseInt(process.env.DATABASE_PORT) || 5432,
+			username: process.env.DATABASE_USER,
+			password: process.env.DATABASE_PASS,
+			database: process.env.DATABASE_NAME,
+			entities: [
+				`${__dirname}/**/*.entity{.ts,.js}`
+			],
+			synchronize: process.env.NODE_ENV !== "production"
+		})
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
