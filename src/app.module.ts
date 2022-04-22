a12294e24d9dba109ce8618788e461c5815bf627
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2022-04-22 22:50:32 +0100

    Setup Auth0 Integration (#15)
    * Add Auth0 environment variables to example.
    
    * Install dependencies required for auth.
    
    * Create auth module (doesn't work)
    
    * A fully working Auth0 implementation.
    
    * Uncomment database module.
    
    * Return JSON and use double quotes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -18,6 +19,7 @@ import { UserModule } from './user/user.module';
       ],
       synchronize: process.env.NODE_ENV !== 'production',
     }),
+    AuthModule,
     UserModule
   ],
   controllers: [AppController],
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from "@nestjs/passport";
+import { JWTStrategy } from "./jwt.strategy";
+
+@Module({
+	imports: [PassportModule.register({ defaultStrategy: "jwt" })],
+	providers: [JWTStrategy],
+	exports: [PassportModule]
+})
+export class AuthModule {}
