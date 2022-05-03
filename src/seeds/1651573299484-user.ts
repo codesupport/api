8d3cd06a4e251316a721bdd45b0202329774fa3b
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-05-03 12:37:50 +0200

    Added user in the seed

--- src/seeds/1651573299484-user.ts
+++ src/seeds/1651573299484-user.ts
@@ -2,8 +2,10 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class user1651573299484 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query("insert into \"user\"(id, auth_id, username) VALUES (1, 'auth_id:test', 'testuser')");
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query("delete from \"user\" where id = 1");
 	}
 }
