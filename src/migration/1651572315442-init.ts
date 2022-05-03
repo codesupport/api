a056d0d7ec95a4f48cd2b115f4aa64bf48c7c04f
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-05-03 12:24:03 +0200

    Fixed linting

--- src/migration/1651572315442-init.ts
+++ src/migration/1651572315442-init.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class init1651572315442 implements MigrationInterface {
-    name = 'init1651572315442'
+	name = "init1651572315442";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "auth_id" character varying NOT NULL, "username" character varying NOT NULL, "created" TIMESTAMP NOT NULL DEFAULT now(), "modified" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_56d00ec31dc3eed1c3f6bff4f58" UNIQUE ("auth_id"), CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query("CREATE TABLE \"user\" (\"id\" SERIAL NOT NULL, \"auth_id\" character varying NOT NULL, \"username\" character varying NOT NULL, \"created\" TIMESTAMP NOT NULL DEFAULT now(), \"modified\" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT \"UQ_56d00ec31dc3eed1c3f6bff4f58\" UNIQUE (\"auth_id\"), CONSTRAINT \"UQ_78a916df40e02a9deb1c4b75edb\" UNIQUE (\"username\"), CONSTRAINT \"PK_cace4a159ff9f2512dd42373760\" PRIMARY KEY (\"id\"))");
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query("DROP TABLE \"user\"");
+	}
 }
--- src/seeds/1651573299484-user.ts
+++ src/seeds/1651573299484-user.ts
@@ -1,11 +1,9 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class user1651573299484 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+	}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+	public async down(queryRunner: QueryRunner): Promise<void> {
+	}
 }
