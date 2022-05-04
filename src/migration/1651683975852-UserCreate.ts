b40367977fc0a595d3de4a6b36e54cadea24187f
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-05-04 19:07:39 +0200

    Updated name for migration file

--- src/migration/1651683975852-UserCreate.ts
+++ src/migration/1651683975852-UserCreate.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class init1651572315442 implements MigrationInterface {
-	name = "init1651572315442";
+export class UserCreate1651683975852 implements MigrationInterface {
+	name = "UserCreate1651683975852";
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query("CREATE TABLE \"user\" (\"id\" SERIAL NOT NULL, \"auth_id\" character varying NOT NULL, \"username\" character varying NOT NULL, \"created\" TIMESTAMP NOT NULL DEFAULT now(), \"modified\" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT \"UQ_56d00ec31dc3eed1c3f6bff4f58\" UNIQUE (\"auth_id\"), CONSTRAINT \"UQ_78a916df40e02a9deb1c4b75edb\" UNIQUE (\"username\"), CONSTRAINT \"PK_cace4a159ff9f2512dd42373760\" PRIMARY KEY (\"id\"))");
--- src/seeds/1651573299484-User.ts
+++ src/seeds/1651573299484-User.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class user1651573299484 implements MigrationInterface {
+export class User1651573299484 implements MigrationInterface {
 	public async up(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query("insert into \"user\"(id, auth_id, username) VALUES (1, 'auth_id:test', 'testuser')");
 	}
