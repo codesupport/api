8b3bc81aaada8527d32916af733c19a9e30fe9b9
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2022-07-22 23:46:56 +0100

    Add the ability to get a singular APPROVED article. (#36)

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, ForbiddenException, Post, Req, UseGuards } from "@nestjs/common";
+import {
+	Body,
+	Controller,
+	ForbiddenException,
+	Get,
+	Post,
+	Param,
+	Req,
+	UseGuards,
+	NotFoundException
+} from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { ApiBearerAuth } from "@nestjs/swagger";
 import { ArticleService } from "./article.service";
@@ -6,6 +16,7 @@ import { CreateArticleDTO } from "./dto/create-article.dto";
 import { ArticleDTO } from "./dto/article.dto";
 import { UserService } from "../user/user.service";
 import AuthenticatedRequest from "../auth/authenticated-request.interface";
+import { ArticleStatus } from "./article.entity";
 
 @Controller("article")
 export class ArticleController {
@@ -13,6 +24,28 @@ export class ArticleController {
 		private readonly articleService: ArticleService,
 		private readonly userService: UserService
 	) {}
+
+	@Get("/:id")
+	async getArticle(@Param("id") articleId: number): Promise<ArticleDTO> {
+		const article = await this.articleService.getArticleByID(articleId);
+
+		if (!article || article.status !== ArticleStatus.APPROVED) {
+			throw new NotFoundException();
+		}
+
+		delete article.user.auth_id;
+
+		return {
+			...article,
+			user: {
+				...article.user,
+				created: article.user.created.toISOString(),
+				modified: article.user.modified.toISOString()
+			},
+			created: article.created.toISOString(),
+			modified: article.modified.toISOString()
+		};
+	}
 
 	@Post()
 	@UseGuards(AuthGuard("jwt"))
